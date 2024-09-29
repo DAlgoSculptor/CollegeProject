8ae1698e1edde2ea1ff97d5e17fc6f5bc4f2852a
DAlgoSculptor <danishnawaz345678@gmail.com>
2024-09-29 16:54:57 +0530

    done

--- CollegeProject/settings.py
+++ CollegeProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o*=jbdd!9=hwu=k#86-7+wu*&1pyd%mxk)20fqg75_gku535fe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
